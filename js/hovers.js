d600e60e8f7c37ffe1e5ad1b540f234f3785695e
Nick <godgiven.nick@gmail.com>
2018-08-20 08:31:08 +0300

    Add files via upload

---
+++ js/hovers.js
@@ -0,0 +1,21 @@
+'use strict';
+(function () {
+
+
+	// var btn = $('.btn').addClass('__hover');
+	var btn = Array.from(document.querySelectorAll('.btn'));
+	var container = document.querySelector('.container');
+
+	var descWidth = 1024;
+
+	// btn.forEach( function(el){
+	// 	if(container.style.width > descWidth){
+	// 		el.classList.add('__hover');
+	// 	}
+	// 	// else {
+	// 	// 	el.classList.remove('__hover');
+	// 	// }
+	// });
+
+
+})(); // the end
